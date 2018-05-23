6b094389ab404e09b2708ed6231a2f5c1bb6f2b0
jealvarez123 <jealvarez123@gmail.com>
2018-05-23 10:51:25 -0500

    created a show.html file to house all the cats

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,7 @@ from .models import Cat
 def index(request):
     cats = Cat.objects.all()
     return render(request, 'index.html', { 'cats':cats })
+
+def show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'show.html', {'cat': cat})
