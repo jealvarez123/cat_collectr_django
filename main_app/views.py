e73cc5ccb6a86732a525ba8a4609d9f620e5c686
jealvarez123 <jealvarez123@gmail.com>
2018-05-23 10:25:25 -0500

    completed part one

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# def index(request):
-#     return render(request, 'index.html')
+from .models import Cat
 
 def index(request):
-    return render(request, 'index.html', {'cats': cats})
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+    cats = Cat.objects.all()
+    return render(request, 'index.html', { 'cats':cats })
